79bcc1ca976998079305b1596f96d775fd51253e
Alexsandr <honorcpro47@gmail.com>
2025-10-25 14:26:11 +0300

    вывод карты

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define mapWidth 80
+#define mapHeight 25
+
+char map[mapHeight][mapWidth + 1];
+
+void ClearMap(){
+    for (int i = 0; i < mapWidth; i++){
+        map[0][i] = '.';
+    }
+    map[0][mapWidth] = '\0';
+    for (int j = 1; j < mapHeight; j++){
+        sprintf(map[j], map[0]);
+    }
+}
+
+void ShowMap(){
+    map[mapHeight - 1][mapHeight - 1] = '\n';
+    for (int j = 0; j < mapHeight; j++){
+        printf("%s", map[j]);
+    }
+}
 
 int main(){
-    std::cout << 3;
+    ClearMap();
+    ShowMap();
 }
